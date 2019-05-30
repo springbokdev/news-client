119de283aa3f4b9437b551a3131436d4ffd30ef2
John Spangenberg <dev@springbok.space>
2019-05-30 16:12:16 +0200

    First commit

---
+++ src/main/java/space/springbok/newsclient/NewsClientApplication.java
@@ -0,0 +1,42 @@
+package space.springbok.newsclient;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class NewsClientApplication {
+
+    @Autowired
+    private EurekaClient client;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsClientApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String callService() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        InstanceInfo instanceInfo = client.getNextServerFromEureka("news-service", false);
+        String baseUrl = instanceInfo.getHomePageUrl();
+        ResponseEntity<String> response = restTemplate.exchange(baseUrl, HttpMethod.GET, null, String.class);
+
+        return response.getBody();
+
+    }
+
+}
---
+++ src/test/java/space/springbok/newsclient/NewsClientApplicationTests.java
@@ -0,0 +1,16 @@
+package space.springbok.newsclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
