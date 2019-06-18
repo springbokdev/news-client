5381ea0de07b637e468d9284647f89f8fec0889c
John Spangenberg <dev@springbok.space>
2019-06-18 17:27:56 +0200

    Enabled Ribbon client-side load-balancing.

--- src/main/java/space/springbok/newsclient/NewsClientApplication.java
+++ src/main/java/space/springbok/newsclient/NewsClientApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +26,16 @@ public class NewsClientApplication {
     @Autowired
     private RestTemplateBuilder restTemplateBuilder;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(NewsClientApplication.class, args);
     }
 
     @RequestMapping("/")
     public String callService() {
+        // 1. with Eureka client and service discovery
         RestTemplate restTemplate = restTemplateBuilder.build();
         InstanceInfo instanceInfo = client.getNextServerFromEureka("news-service", false);
         String baseUrl = instanceInfo.getHomePageUrl();
@@ -37,6 +43,15 @@ public class NewsClientApplication {
 
         return response.getBody();
 
+        // 2. with Ribbon and service discovery
+//        return restTemplate.getForEntity("http://news-service", String.class).getBody();
+
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
